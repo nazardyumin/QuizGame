d3a8ad5758940e09fa364fca04843a2e9464d220
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2022-08-18 21:10:28 +0500

    Refactored User.cs

--- Users/User.cs
+++ Users/User.cs
@@ -1,11 +1,14 @@
-﻿public class User
+namespace QuizGame.Users
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string DateOfBirth { get; set; }
-    public string Login { get; set; }
-    public string Password { get; set; }
-    public bool IsAdmin { get; set; }
-    public bool IsSuperAdmin { get; set; }
-    public List<QuizResult>? Results { get; set; }
+    public class User
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsSuperAdmin { get; set; }
+        public List<QuizResult>? Results { get; set; }
+    }
 }
